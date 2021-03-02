63b7dde9d8c15a972ea6099656ffe920f7fd3939
alibudi <35588270+alibudi@users.noreply.github.com>
2021-03-02 10:37:57 +0700

    Update main.js

--- main.js
+++ main.js
@@ -4,30 +4,31 @@
 // };
 
 window.onload = () => {
+  (function (i, s, o, g, r, a, m) {
+    i["GoogleAnalyticsObject"] = r;
+    (i[r] =
+      i[r] ||
+      function () {
+        (i[r].q = i[r].q || []).push(arguments);
+      }),
+      (i[r].l = 1 * new Date());
+    (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m);
+  })(
+    window,
+    document,
+    "script",
+    "https://www.google-analytics.com/analytics.js",
+    "ga"
+  );
+
+  ga("create", "G-KVWZZZL2XD", "auto");
+  ga("send", "pageview");
   myFunction();
 };
-(function (i, s, o, g, r, a, m) {
-  i["GoogleAnalyticsObject"] = r;
-  (i[r] =
-    i[r] ||
-    function () {
-      (i[r].q = i[r].q || []).push(arguments);
-    }),
-    (i[r].l = 1 * new Date());
-  (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
-  a.async = 1;
-  a.src = g;
-  m.parentNode.insertBefore(a, m);
-})(
-  window,
-  document,
-  "script",
-  "https://www.google-analytics.com/analytics.js",
-  "ga"
-);
 
-ga("create", "G-KVWZZZL2XD", "auto");
-ga("send", "pageview");
 function myFunction() {
   var a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   var n;
